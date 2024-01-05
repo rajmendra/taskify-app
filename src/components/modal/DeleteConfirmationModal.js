09cc252961e1f8cd04380c2fcefee84c29134eec
Rajmendra <rajmendra.rawat@gmail.com>
2024-01-05 17:13:56 +0530

    fix: test vercel deploy

--- src/components/modal/DeleteConfirmationModal.js
+++ src/components/modal/DeleteConfirmationModal.js
@@ -4,7 +4,7 @@ const DeleteConfirmationModal = ({ onDelete, onCancel }) => {
   return (
     <div className="overlay">
       <div className="delete-confirmation">
-        <p>Are you sure you want to delete this task?</p>
+        <p>Are you sure you want to delete this task? </p>
         <button className="button btn-primary" onClick={onDelete}>
           Yes
         </button>
